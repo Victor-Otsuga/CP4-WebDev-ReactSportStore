dd9e121c8257b73b097d141edb9173e8cae38203
Victor Augusto Pereira dos Santos <victorsantosp10@hotmail.com>
2024-04-02 14:44:59 -0300

    feat: index almost done

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,7 +1,10 @@
 import {} from "react";
 import "../css/estilo.css"
-import BrandVideo from '../assets/midia/fashion.mp4'
-import CampTwo from '../assets/midia/campanha2.png'
+import Sport1 from '../assets/sport1.jpg'
+import Sport2 from '../assets/sport2.jpg'
+import Sport3 from '../assets/sport3.jpg'
+import Sport4 from '../assets/sport4.jpg'
+
 
 function Home() {
   return (
@@ -9,15 +12,7 @@ function Home() {
       <main class="conteudo">
         <section class="conteudo-central">
           <div class="midia">
-            <video
-              width="25%"
-              height=""
-              autoplay
-              muted
-              loop
-              src={BrandVideo}
-              type="video/mp4"
-            ></video>
+          <img src={Sport2} alt="Ruisse" />
             <div id="texto-conteudo">
               <h1>(im)PERFEIÇÃO DE RUA</h1>
               <p id="resumo-conteudo">
@@ -28,9 +23,47 @@ function Home() {
           </div>
         </section>
 
+        <section class="products">
+        <a class="nostyle" href="./product-01card.html">
+          <div class="product">
+            <img src={Sport4} alt="Ruisse" />
+            <h2>Cropped Moletinho</h2>
+            <p>Cropped Moletinho e RIB, produzido em Poliéster.</p>
+            <p class="price">R$ 829,41</p>
+            <button>Comprar</button>
+          </div>
+        </a>
+        <a class="nostyle" href="./product-02card.html">
+          <div class="product">
+            <img src={Sport4}  alt="Ruisse" />
+            <h2>Vestido Moletinho</h2>
+            <p>Vestido tecido de moletinho e RIB.</p>
+            <p class="price">R$ 1.153,36</p>
+            <button>Comprar</button>
+          </div>
+        </a>
+        <a class="nostyle" href="./product-03card.html">
+          <div class="product">
+            <img src={Sport4}  alt="Ruisse" />
+            <h2>Camisa Alongada</h2>
+            <p>Camisa de algodão com modelagem reta.</p>
+            <p class="price">R$ 610,76</p>
+            <button>Comprar</button>
+          </div>
+        </a>
+        <a class="nostyle" href="./product-04card.html">
+          <div class="product">
+            <img src={Sport4}  alt="Ruisse" />
+            <h2>Top Cinto</h2>
+            <p>Top de sarja leve com abertura frontal em zíper tratorado.</p>
+            <p class="price">R$ 652,48</p>
+            <button>Comprar</button>
+          </div>
+        </a>
+      </section>
 
         <section class="conteudo-secundario">
-          <img src={CampTwo} alt="Ruisse" />
+        <img src={Sport3 } alt="Ruisse" />
         </section>
 
       </main>
